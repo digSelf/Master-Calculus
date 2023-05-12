b5a3635a9c9a09d73c5f13a1ac7c0b321e625c4e
iforget <coding@algo.ac.cn>
2023-05-12 10:27:58 +0800

    rename

--- 03.Derivative/01-global-slope.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.plot([-1, 3], [1, 6], 'o')
-plt.plot([-1, 3], [1, 6], color='k')
-
-slope = (6 - 1) / (3 - (-1))
-
-plt.title(f"the slope is {slope}")
-plt.show()
